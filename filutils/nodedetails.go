72e45ae268e6b7142a43e41a5ed8143366b3945f
ceuloong <xuyloong@gmail.com>
2024-07-12 13:34:55 +0800

    filutils

---
+++ filutils/nodedetails.go
@@ -0,0 +1,447 @@
+package httputils
+
+import (
+	"blog/models"
+	"blog/services"
+	"fmt"
+	"github.com/anaskhan96/soup"
+	"github.com/shopspring/decimal"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func GetHttpContent(requestUrl string) string {
+	//requestUrl := "https://www.liaoxuefeng.com/"
+	// 发送Get请求
+	rsp, err := http.Get(requestUrl)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+
+	body, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	content := string(body)
+	defer rsp.Body.Close()
+
+	return content
+}
+
+// GetAllNodeByAddress 获取当前node下所有的节点名称
+func GetAllNodeByAddress(node string) []models.Nodes {
+	content := GetHttpContent("https://filfox.info/zh/address/" + node)
+	// 下面主要是解析标签
+	doc := soup.HTMLParse(content)
+	subDocs := doc.FindAll("dl", "class", "flex")
+	var nodes []models.Nodes
+	for _, subDoc := range subDocs {
+		link := subDoc.Find("dt")
+		if link.Error != nil {
+			//log.Println(link.Error)
+			continue
+		}
+		if strings.TrimSpace(link.Text()) == "名下存储提供者" {
+			fmt.Println(strings.TrimSpace(link.Text()))
+			providers := subDoc.FindAll("p")
+			for _, provide := range providers {
+				nodeStr := provide.Find("a")
+				if nodeStr.Error != nil {
+					continue
+				}
+				oneNode := models.Nodes{
+					Node:       strings.TrimSpace(nodeStr.Text()),
+					MsigNode:   node,
+					CreateTime: time.Now(),
+					LastTime:   time.Now(),
+				}
+				nodes = append(nodes, oneNode)
+			}
+		}
+	}
+	return nodes
+}
+
+// GetNodeDetailByAddress 根据节点名称查询节点详细信息
+func GetNodeDetailByAddress(nodes models.Nodes) models.Nodes {
+	node := nodes.Node
+	content := GetHttpContent("https://filfox.info/zh/address/" + node)
+	// 下面主要是解析标签
+	doc := soup.HTMLParse(content)
+	subDocs := doc.FindAll("div", "class", "rounded-md")
+	//var m = make(map[string]string)
+	//var nodes models.Nodes
+	var _thisLast int
+	for _, subDoc := range subDocs {
+		links := subDoc.FindAll("p")
+		for _, link := range links {
+			if link.Error != nil {
+				//log.Println(link.Error)
+				continue
+			}
+			var text = strings.TrimSpace(link.Text())
+			if text == "账户余额" {
+				balance := strings.TrimSpace(link.FindNextElementSibling().Text())
+				nodes.Balance = DecimalValue(FormaterString(strings.Split(balance, " ")[0]))
+				if strings.Split(balance, " ")[1] != "FIL" {
+					nodes.Balance = decimal.Zero
+				}
+				continue
+			}
+			// 可用余额页面抓取有问题，采用请求的方式获得
+			/*if strings.Contains(text, "可用余额") {
+				s := link.Children()
+				for _, value := range s {
+					if value.NodeValue == "span" {
+						v := value.FindNextSibling().Text()
+						split := strings.Split(strings.TrimSpace(strings.ReplaceAll(v, ":", "")), " ")
+						nodes.AvailableBalance = DecimalValue(split[0])
+					}
+				}
+				continue
+			}*/
+			if strings.Contains(text, "扇区抵押") {
+				//m["扇区抵押"] = strings.SplitAfter(text, ":")[1]
+				pledge := strings.TrimSpace(strings.SplitAfter(text, ":")[1])
+				nodes.SectorPledgeBalance = DecimalValue(FormaterString(strings.Split(pledge, " ")[0]))
+				continue
+			}
+			if strings.Contains(text, "提供存储服务锁仓") {
+				//m["提供存储服务锁仓"] = strings.SplitAfter(text, ":")[1]
+				funds := strings.TrimSpace(strings.SplitAfter(text, ":")[1])
+				nodes.VestingFunds = DecimalValue(FormaterString(strings.Split(funds, " ")[0]))
+				continue
+			}
+			if text == "有效算力" {
+				_thisLast = 2
+				continue
+			}
+			if _thisLast == 2 {
+				//m["有效算力"] = text
+				_thisLast = 0
+				nodes.QualityAdjPower = DecimalValue(FormaterString(strings.Split(text, " ")[0]))
+				nodes.PowerUnit = strings.TrimSpace(strings.Split(text, " ")[1])
+				if nodes.PowerUnit == "TiB" {
+					nodes.QualityAdjPower = nodes.QualityAdjPower.Div(DecimalValue("1000")).Round(2)
+					nodes.PowerUnit = "PiB"
+				} else if nodes.PowerUnit == "GiB" {
+					nodes.QualityAdjPower = nodes.QualityAdjPower.Div(DecimalValue("1000000")).Round(2)
+					nodes.PowerUnit = "PiB"
+				}
+
+				continue
+			}
+			if strings.Contains(text, "占比") {
+				//m["算力占比"] = strings.SplitAfter(text, ":")[1]
+				point := strings.Split(text, ":")[1]
+				nodes.PowerPoint = DecimalValue(FormaterString(strings.Split(point, "%")[0]))
+				continue
+			}
+			if strings.Contains(text, "排名") {
+				//m["排名"] = strings.SplitAfter(text, ":")[1]
+				nodes.PowerGrade = strings.TrimSpace(strings.SplitAfter(text, ":")[1])
+				continue
+			}
+			if strings.Contains(text, "原值算力") {
+				s := link.FindNextElementSibling().Children()
+				for i, value := range s {
+					if i == 2 {
+						v := strings.TrimSpace(strings.ReplaceAll(value.NodeValue, ":", ""))
+						num, _ := strconv.Atoi(v)
+						nodes.WeightedBlocks = num
+					}
+				}
+
+				continue
+			}
+
+			if strings.Contains(text, "累计出块奖励") {
+				//m["累计出块奖励"] = strings.SplitAfter(text, ":")[1]
+				reward := strings.TrimSpace(strings.SplitAfter(text, ":")[1])
+				nodes.RewardValue = DecimalValue(FormaterString(strings.SplitAfter(reward, " ")[0]))
+				continue
+			}
+			if strings.Contains(text, "扇区大小") {
+				//m["扇区大小"] = strings.SplitAfter(text, ":")[1]
+				nodes.SectorSize = strings.TrimSpace(strings.SplitAfter(text, ":")[1])
+				continue
+			}
+			if strings.Contains(text, "扇区状态") {
+				sectorStatus := link.FindNextElementSibling().FindAll("span")
+				var stas string
+				for _, sta := range sectorStatus {
+					stas = stas + sta.Text()
+				}
+				for _, sta := range sectorStatus {
+					split := strings.Split(strings.TrimSpace(strings.ReplaceAll(sta.Text(), ",", "")), " ")
+					num, _ := strconv.Atoi(split[0])
+					if split[1] == "全部" {
+						nodes.SectorTotal = num
+					} else if split[1] == "有效" {
+						nodes.SectorEffective = num
+					} else if split[1] == "错误" {
+						nodes.SectorError = num
+					} else {
+						nodes.SectorRecovering = num
+					}
+
+				}
+				//m["扇区状态"] = stas
+				nodes.SectorStatus = strings.TrimSpace(stas)
+				continue
+			}
+			if strings.Contains(text, "地址:") {
+				//m["地址"] = link.FindNextElementSibling().Text()
+				nodes.Address = strings.TrimSpace(link.FindNextElementSibling().Text())
+				continue
+			}
+			if strings.Contains(text, "消息数") {
+				m := link.FindNextElementSibling().Text()
+				count, _ := strconv.Atoi(FormaterString(m))
+				nodes.MsgCount = count
+				continue
+			}
+			if strings.Contains(text, "类型:") {
+				//m["类型"] = link.FindNextElementSibling().Text()
+				nodes.SectorType = strings.TrimSpace(link.FindNextElementSibling().Text())
+				continue
+			}
+			if strings.Contains(text, "创建时间") {
+				//m["创建时间"] = link.FindNextElementSibling().Text()
+				nodes.CreateTime = StringToTime(strings.TrimSpace(link.FindNextElementSibling().Text()))
+				continue
+			}
+		}
+	}
+	return nodes
+}
+
+// GetControlDetailByAddress 根据地址查询详细信息
+func GetControlDetailByAddress(addr string) Account {
+	content := GetHttpContent("https://filfox.info/zh/address/" + addr)
+	// 下面主要是解析标签
+	doc := soup.HTMLParse(content)
+	subDocs := doc.FindAll("div", "class", "rounded-md")
+	var account Account
+	for _, subDoc := range subDocs {
+		links := subDoc.FindAll("dl")
+		for _, link := range links {
+			if link.Error != nil {
+				//log.Println(link.Error)
+				continue
+			}
+			text := strings.TrimSpace(link.Find("dt").Text())
+			if text == "余额" {
+				balance := strings.TrimSpace(link.FindNextElementSibling().Text())
+				account.ControlBalance = DecimalValue(FormaterString(strings.Split(balance, " ")[0]))
+				break
+			}
+		}
+		break
+	}
+	return account
+}
+
+func SaveNodes(node string) {
+	nodes := GetAllNodeByAddress(node)
+	if len(nodes) > 0 {
+		services.SaveNode(nodes)
+	}
+}
+
+func UpdateNodes(nodeParam string, timeTag int64) {
+	nodes := services.FindAllNode(nodeParam)
+	var hasPowerCount int
+	var noPowerCount int
+
+	var savePool bool
+
+	for _, oneNode := range nodes {
+		if TimeAddMinutes(oneNode.LastTime, 30).Compare(time.Now()) > 0 {
+			continue
+		}
+		savePool = true
+		var needMysql bool
+
+		// 获取节点详细数据
+		log.Printf("获取节点%s信息\n", oneNode.Node)
+		n := GetNodeDetailByAddress(oneNode)
+		//n.Node = oneNode.Node
+		time.Sleep(5 * time.Second)
+
+		// 获取节点可用余额数据
+		log.Printf("获取节点%s可用余额\n", oneNode.Node)
+		account := BalanceStats(oneNode.Node)
+		n.AvailableBalance = account.AvailableBalance
+		n.Height = account.Height
+		n.LastTime = time.Now() //TimestampToTime(account.LastTime)
+		//fmt.Printf("nodes=%+v\n", n)
+		time.Sleep(5 * time.Second)
+
+		if !n.Balance.IsZero() || oneNode.Balance.IsZero() && oneNode.LuckyValue24h.GreaterThan(decimal.Zero) {
+			needMysql = true
+
+			log.Printf("获取节点%s的24hminer状态\n", oneNode.Node)
+			miningDetail := MiningStats(oneNode.Node)
+			n.BlocksMined24h = miningDetail.BlocksMined
+			n.WeightedBlocksMined24h = miningDetail.WeightedBlocksMined
+			n.TotalRewards24h = DecimalDiv18Value(miningDetail.TotalRewards)
+			n.LuckyValue24h = decimal.NewFromFloat(miningDetail.LuckyValue)
+			n.QualityAdjPowerDelta24h = DecimalDiv1024x4Value(miningDetail.QualityAdjPowerDelta)
+			time.Sleep(5 * time.Second)
+
+			/*log.Printf("获取节点%s的7dminer状态\n", oneNode.Node)
+			miningDetail = MiningStatsCycle(oneNode.Node, "7d")
+			n.BlocksMined7d = miningDetail.BlocksMined
+			n.WeightedBlocksMined7d = miningDetail.WeightedBlocksMined
+			n.TotalRewards7d = DecimalDiv18Value(miningDetail.TotalRewards)
+			n.LuckyValue7d = decimal.NewFromFloat(miningDetail.LuckyValue)
+			n.QualityAdjPowerDelta7d = DecimalDiv1024x4Value(miningDetail.QualityAdjPowerDelta)
+			time.Sleep(5 * time.Second)
+
+			log.Printf("获取节点%s的30dminer状态\n", oneNode.Node)
+			miningDetail = MiningStatsCycle(oneNode.Node, "30d")
+			n.BlocksMined30d = miningDetail.BlocksMined
+			n.WeightedBlocksMined30d = miningDetail.WeightedBlocksMined
+			n.TotalRewards30d = DecimalDiv18Value(miningDetail.TotalRewards)
+			n.LuckyValue30d = decimal.NewFromFloat(miningDetail.LuckyValue)
+			n.QualityAdjPowerDelta30d = DecimalDiv1024x4Value(miningDetail.QualityAdjPowerDelta)
+			time.Sleep(5 * time.Second)*/
+		}
+
+		// 获取节点的控制地址 独立方法获取 独立表保存，一个节点存在多个控制地址
+		/*if len(n.ControlAddress) == 0 {
+			log.Printf("获取节点%s控制地址\n", oneNode.Node)
+			controlAddress := SpiderMessage(oneNode.Node)
+			n.ControlAddress = controlAddress
+			time.Sleep(5 * time.Second)
+		}*/
+
+		// 获取控制地址的余额
+		//log.Printf("获取节点%s控制地址余额\n", oneNode.Node)
+		//if len(n.ControlAddress) > 0 {
+		//	account = BalanceControl(n.ControlAddress)
+		//	n.ControlBalance = account.ControlBalance
+		//}
+
+		if n.Height > 0 {
+			if n.LastDistributeTime.IsZero() {
+				n.LastDistributeTime = time.Now()
+			}
+			if needMysql {
+				transAmount := services.SumReward(n, timeTag)
+				str := fmt.Sprintf("%f", transAmount)
+				log.Printf("上次分币%s之后一共转出%s", n.LastDistributeTime, str)
+				n.HasTransfer = n.HasTransfer.Add(DecimalValue(str))
+
+				// 获取节点的转入转出销毁数量
+				mapA := services.SumValueByType(n.Node, timeTag)
+				if value, ok := mapA["receive"]; ok {
+					n.ReceiveAmount = n.ReceiveAmount.Add(value)
+				}
+				if value, ok := mapA["burn"]; ok {
+					n.BurnAmount = n.BurnAmount.Add(value)
+				}
+				if value, ok := mapA["send"]; ok {
+					n.SendAmount = n.SendAmount.Add(value)
+				}
+
+				//s := services.LuckyBlock{}
+				//var count int64
+				//s.CountByNodeTimeTag(oneNode.Node, timeTag, &count)
+				n.TransferCount = oneNode.RealCount // 对齐交易的数量
+			}
+			n.TimeTag = timeTag
+			services.UpdateNode(n)
+			// 保存图表数据
+			services.SaveNodesChart(n)
+			time.Sleep(15 * time.Second)
+		}
+		if n.QualityAdjPower.IsZero() {
+			noPowerCount++
+		} else {
+			hasPowerCount++
+		}
+
+	}
+
+	// 保存矿池图表数据
+	if savePool {
+		log.Printf("一共更新的 %d 个节点，其中有算力的节点 %d 个, 算力为0的节点 %d 个。\n", len(nodes), hasPowerCount, noPowerCount)
+		savePoolChart()
+	} else {
+		log.Printf("没有需要更新的节点。")
+	}
+}
+
+func savePoolChart() {
+	nodes := services.FindAllNode("")
+	poolChart := new(models.PoolChart)
+
+	for _, n := range nodes {
+		poolChart.Balance = poolChart.Balance.Add(n.Balance)
+		poolChart.AvailableBalance = poolChart.AvailableBalance.Add(n.AvailableBalance)
+		poolChart.SectorPledgeBalance = poolChart.SectorPledgeBalance.Add(n.SectorPledgeBalance)
+		poolChart.VestingFunds = poolChart.VestingFunds.Add(n.VestingFunds)
+		poolChart.QualityAdjPower = poolChart.QualityAdjPower.Add(n.QualityAdjPower)
+		poolChart.PowerPoint = poolChart.PowerPoint.Add(n.PowerPoint)
+		poolChart.ControlBalance = poolChart.ControlBalance.Add(n.ControlBalance)
+		poolChart.RewardValue = poolChart.RewardValue.Add(n.RewardValue)
+
+		//nodesChart := services.GetNodesChart(n)
+
+	}
+	poolChart.LastTime = time.Now()
+	poolChart.PowerUnit = "PiB"
+	services.SavePoolChart(poolChart)
+}
+
+func HandUpdate(nodeParam string) {
+	nodes := services.FindAllNode(nodeParam)
+
+	var contents string
+
+	for _, oneNode := range nodes {
+		//if TimeAddMinutes(oneNode.LastHandTime, 30)
+		if oneNode.LastHandTime.Add(time.Minute*30).Compare(time.Now()) > 0 || oneNode.Balance.IsZero() {
+			continue
+		}
+		// 获取节点详细数据
+		log.Printf("获取节点%s信息\n", oneNode.Node)
+		n := GetNodeDetailByAddress(oneNode)
+		//n.Node = oneNode.Node
+		time.Sleep(5 * time.Second)
+
+		// 获取节点可用余额数据
+		log.Printf("获取节点%s可用余额\n", oneNode.Node)
+		account := BalanceStats(oneNode.Node)
+		n.AvailableBalance = account.AvailableBalance
+		n.Height = account.Height
+		now := time.Now()
+		n.LastHandTime = &now //TimestampToTime(account.LastTime)
+		//fmt.Printf("nodes=%+v\n", n)
+		time.Sleep(5 * time.Second)
+
+		services.UpdateNode(n)
+
+		if n.SectorError > oneNode.SectorError && (n.SectorError-oneNode.SectorError > 100) {
+			contents += fmt.Sprintf("节点：%s 当前算力：%s %s，扇区状态：%s，错误扇区数量增加 %d。<br/>", n.Node, n.QualityAdjPower.String(), n.PowerUnit, n.SectorStatus, n.SectorError-oneNode.SectorError)
+		}
+	}
+	if len(contents) > 0 {
+		var msg models.Msg
+		msg.Title = "扇区错误"
+		msg.Content = contents
+		msg.CreateTime = time.Now()
+		msg.SendStatus = 0
+		services.InsertMsg(msg)
+	}
+
+	log.Printf("手动更新节点余额成功。")
+}
---
+++ filutils/transfer.go
@@ -0,0 +1,484 @@
+package filutils
+
+import (
+	"blog/httputils"
+	"blog/models"
+	"blog/services"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// Result 返回的结构体
+type Result struct {
+	TotalCount int64 `json:"totalCount"`
+	Transfers  []struct {
+		Height    int64  `json:"height"`
+		Timestamp int64  `json:"timestamp"`
+		From      string `json:"from"`
+		To        string `json:"to"`
+		Message   string `json:"message"`
+		Value     string `json:"value"`
+		Type      string `json:"type"`
+	} `json:"transfers"`
+	Types []string `json:"types"`
+}
+
+// UpdateDetail 结构体
+type UpdateDetail struct { //
+	services.LuckyBlock
+	page int
+}
+
+type SpiderNode struct {
+	Node     string
+	PidNode  string
+	TimeTag  int64
+	Category string
+	Page     int
+}
+
+var wg sync.WaitGroup
+
+const pageSize = 100
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile | log.Lmsgprefix)
+	// 通过SetPrefix设置Logger结构体里的prefix属性
+	log.SetPrefix("INFO:")
+}
+
+func Start(timeTag int64, nodeParam string) {
+	// f17225euatkghukeyu6exm6j72fw5aneiiyapotbq  f1emjr5wnycomu7dgbt6aiaiir6rn53ggioiou56a
+	//f01807413 f01822659 f01845913  f01874748  f01834260  f01899511
+	//nodes := []string{"f01807413", "f01822659", "f01845913", "f01874748", "f01834260", "f01899511"}
+	nodes := services.FindAllNode(nodeParam)
+
+	pg := 5
+	page := pageCount(len(nodes), pg)
+	for p := 1; p <= page; p++ {
+		pageNode(nodes, p, pg, timeTag)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func pageNode(nodes []models.Nodes, p int, pg int, timeTag int64) {
+	s := services.LuckyBlock{}
+	var newNodes []SpiderNode
+	var updateNodes []SpiderNode
+	//var m = make(map[string]int)
+
+	index := pg * p
+	if index > len(nodes) {
+		index = len(nodes)
+	}
+
+	for i := (p - 1) * pg; i < index; i++ {
+
+		//for i := 0; i < len(nodes); i++ {
+
+		node := SpiderNode{
+			Node:     nodes[i].Node,
+			Category: "",
+			TimeTag:  timeTag,
+		}
+
+		var count int64
+		count = nodes[i].TransferCount
+		if count == 0 {
+			s.CountByNode(node.Node, &count)
+		}
+
+		//if count == 0 {
+		//	newNodes = append(newNodes, node)
+		//} else {
+		//	if len(newNodes) > 0 {
+		//		continue
+		//	}
+		total := getTotalNum(node.Node) // 获取全部记录数量
+		services.UpdateRealCount(total, node.Node)
+		page := pageCount(total-int(count), pageSize)
+		if page <= 0 {
+			continue
+		}
+		node.Page = page
+		updateNodes = append(updateNodes, node)
+		//}
+	}
+
+	// 抓取控制地址记录
+	/*for i := 0; i < len(nodes); i++ {
+		if nodes[i].Status > 1 && nodes[i].Status < 5 {
+			node := nodes[i].ControlAddress
+			var count int64
+			s.CountByNode(node, &count)
+
+			if count == 0 {
+				newNodes = append(newNodes, node)
+			} else {
+				total := getTotalNum(node) // 获取全部记录数量
+				page := pageCount(total-int(count), pageSize)
+				if page == 0 {
+					continue
+				}
+				m[node] = page
+				updateNodes = append(updateNodes, node)
+			}
+		}
+	}*/
+
+	if len(newNodes) > 0 { // 新节点首次抓数据
+		log.Printf("新节点首次抓数据: %d\n", len(newNodes))
+		newNodeSave(newNodes)
+	} else if len(updateNodes) > 0 { // 已经完整抓取过，实时更新就行
+		log.Printf("更新节点: len(m)= %d\n", len(updateNodes))
+		updateNodeNew(updateNodes)
+	} else {
+		log.Println("no data need to update. next page: ", p+1)
+	}
+}
+
+// 已存在的节点更新数据
+func updateNodeNew(nodes []SpiderNode) {
+	start := time.Now()
+
+	wg.Add(len(nodes))
+	for i := 0; i < len(nodes); i++ {
+		go func(i int) {
+			defer wg.Done()
+			GetHttpHtmlNew(nodes[i])
+		}(i)
+	}
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Printf("updateNodeNew WaitGroupStart Time %s\n ", elapsed)
+}
+
+// 已存在的节点更新数据
+/*func updateNode(m map[string]services.LuckyBlock) {
+	for node, block := range m {
+		var needToSave []models.LuckyBlock
+		needToSave = getSpiders(node, block, 0, needToSave,)
+		if len(needToSave) > 0 {
+			log.Println("保存新数据：", len(needToSave))
+			//services.Insert(needToSave)
+			total := len(needToSave)
+			pageCount := int(math.Ceil(float64(total) / float64(pageSize)))
+			for i := 1; i <= pageCount; i++ {
+				start, end := SlicePage(i, pageSize, total)
+				blocks := needToSave[start:end]
+				services.Insert(blocks)
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+}*/
+
+func getSpiders(node SpiderNode, block services.LuckyBlock, page int, needToSave []models.LuckyBlock) []models.LuckyBlock {
+	time.Sleep(1 * time.Second)
+	total, spiders := Spider(node, page, pageSize, 0)
+	log.Printf("spiders.len: %d,total: %d", len(spiders), total)
+	need := services.NeedToSave(block, spiders)
+	if need == nil {
+		return needToSave
+	}
+
+	needToSave = append(need, needToSave...)
+
+	//if len(needToSave) == 20 {
+	//	getSpiders(node, block, page+1)
+	//} else {
+	//	return needToSave
+	//}
+	return getSpiders(node, block, page+1, needToSave)
+}
+
+// 节点首次抓取数据
+func newNodeSave(nodes []SpiderNode) {
+	log.Println("开始抓取数据")
+	start := time.Now()
+
+	wg.Add(len(nodes))
+	for i := 0; i < len(nodes); i++ {
+		go func(i int) {
+			defer wg.Done()
+			GetHttpHtmlNew(nodes[i])
+		}(i)
+	}
+	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Printf("WaitGroupStart Time %s\n ", elapsed)
+}
+
+// GetHttpHtmlNew 首次抓取数据时执行
+func GetHttpHtmlNew(node SpiderNode) {
+	p := node.Page
+	total := getTotalNum(node.Node) // 获取全部记录数量  8078
+	// 首次抓取数据时100条每页
+	var addPage int
+	var errCount int
+	var lastBlock services.LuckyBlock
+	if p == 0 {
+		p = pageCount(total, pageSize)
+	} else {
+		lastBlock = services.FindLastByNode(node.Node)
+		log.Printf("获取节点%s保存的最后一条数据%T\n", node.Node, lastBlock)
+	}
+	needRemoveRep := true
+
+	for page := p - 1; page >= 0; page-- {
+		var totalBlock []models.LuckyBlock
+		var totalCount int
+		totalCount, totalBlock = Spider(node, page, pageSize, errCount) // 保存数据库
+		if len(totalBlock) == 0 {
+			for i := 0; i < 5; i++ {
+				time.Sleep(5 * time.Second)
+				totalCount, totalBlock = Spider(node, page, pageSize, errCount) // 保存数据库
+				if len(totalBlock) > 0 {
+					break
+				}
+				log.Printf("节点%s查询%d页时，数量为0，for i=%d\n", node, page, i)
+			}
+		}
+		if len(totalBlock) > 0 {
+			if needRemoveRep && lastBlock != (services.LuckyBlock{}) { //保存最后一页时去重
+				totalBlock = services.NeedToSave(lastBlock, totalBlock)
+				log.Printf("更新节点%s时去重，page:%d", node.Node, p)
+			}
+
+			if len(totalBlock) > 0 {
+				needRemoveRep = false
+				services.Insert(totalBlock)
+			}
+		}
+
+		if totalCount > total {
+			if page < totalCount/pageSize {
+				addPage = page + 1 // 加1是再次查询当前页数据，总数量增加了一，页数的位置有变化
+				_, newPage := Spider(node, addPage, pageSize, 0)
+				log.Printf("totalCount:%d, total:%d,数量有变化，需要向前查一页addPage:%d, newPage长度:%d", totalCount, total, addPage, len(newPage))
+				if len(newPage) > 0 {
+					needSave := saveNewPage(newPage, totalCount-total)
+					total = totalCount
+					if len(needSave) > 0 {
+						services.Insert(needSave)
+					}
+				}
+			}
+		}
+		time.Sleep(2 * time.Second)
+		log.Printf("保存%s第%d页数据成功\n", node.Node, page)
+	}
+}
+
+// GetHttpHtml 首次抓取数据时执行
+func GetHttpHtml(node SpiderNode) {
+	GetHttpHtmlNew(node)
+	/*total := getTotalNum(node) // 获取全部记录数量  8078
+	// 首次抓取数据时100条每页
+	pageSize := 100
+	var addPage int
+	var errCount int
+	for page := total / pageSize; page >= 0; page-- {
+		var totalBlock []models.LuckyBlock
+		var totalCount int
+		totalCount, totalBlock = Spider(node, page, pageSize, errCount) // 保存数据库
+		if len(totalBlock) == 0 {
+			for i := 0; i < 5; i++ {
+				time.Sleep(5 * time.Second)
+				totalCount, totalBlock = Spider(node, page, pageSize, errCount) // 保存数据库
+				if len(totalBlock) > 0 {
+					break
+				}
+				log.Printf("节点%s查询%d页时，数量为0，for i=%d\n", node, page, i)
+			}
+		}
+		if len(totalBlock) > 0 {
+			services.Insert(totalBlock)
+		}
+
+		if totalCount > total {
+			if page < totalCount/pageSize {
+				addPage = page + 2 // 加2是需要查当前页的上一页数据
+				_, newPage := Spider(node, addPage, pageSize, 0)
+				log.Printf("totalCount:%d, total:%d,数量有变化，需要向前查一页addPage:%d, newPage长度:%d", totalCount, total, addPage, len(newPage))
+				if len(newPage) > 0 {
+					needSave := saveNewPage(newPage, totalCount-total)
+					total = totalCount
+					if len(needSave) > 0 {
+						services.Insert(needSave)
+					}
+				}
+			}
+		}
+
+		time.Sleep(2 * time.Second)
+		log.Printf("保存%s第%d页数据成功\n", node, page)
+	}*/
+}
+
+// 获取全部的区块数量
+func getTotalNum(node string) int {
+	url := `https://filfox.info/api/v1/address/` + node + `/transfers?pageSize=20&page=0`
+	
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Locale", "zh")
+	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"115\", \"Google Chrome\";v=\"115\", \"Not:A-Brand\";v=\"99\"")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "macOS")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result Result
+	_ = json.Unmarshal(bodyText, &result) //byte to json
+	total := result.TotalCount
+	fmt.Printf("node %s total: %d", node, total)
+	return int(total)
+}
+
+// Spider 传入页数，一页一页爬取
+func Spider(node SpiderNode, page int, pageSize int, errCount int) (int, []models.LuckyBlock) {
+	var tmp []models.LuckyBlock
+	p := strconv.Itoa(page)
+	log.Printf("当前页page:%d, p:%s\n", page, p)
+	client := &http.Client{}
+	url := fmt.Sprintf("https://filfox.info/api/v1/address/%s/transfers?pageSize=%d&page=%d", node.Node, pageSize, page)
+	reqSpider, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reqSpider.Header.Set("Accept", "application/json, text/plain, */*")
+	reqSpider.Header.Set("Locale", "zh")
+	reqSpider.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
+	reqSpider.Header.Set("sec-ch-ua-mobile", "?0")
+	reqSpider.Header.Set("sec-ch-ua-platform", "macOS")
+	reqSpider.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+	respSpider, err := client.Do(reqSpider)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer respSpider.Body.Close()
+	bodyText, err := ioutil.ReadAll(respSpider.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result Result
+	_ = json.Unmarshal(bodyText, &result) //byte to json
+	num := len(result.Transfers)
+
+	/*	if num == 0 && errCount < 10 {
+		log.Printf("查询%s第%d页时为空，重新查询当前页，errCount:%d\n", node, page, errCount)
+		time.Sleep(5 * time.Second)
+		Spider(node, page, pageSize, errCount+1) // 可能超时没有返回数据，当前页数据再查询
+	}*/
+
+	transfers := result.Transfers
+	total := result.TotalCount
+	for i := num - 1; i >= 0; i-- {
+		var luckBlock models.LuckyBlock
+		luckBlock.Height = transfers[i].Height
+		luckBlock.Node = node.Node
+		luckBlock.Date = httputils.TimestampToTime(transfers[i].Timestamp)
+		luckBlock.NodeFrom = transfers[i].From
+		luckBlock.NodeTo = transfers[i].To
+		luckBlock.RewardValue = httputils.DecimalDiv18Value(transfers[i].Value)
+		luckBlock.Message = transfers[i].Message
+		luckBlock.Type = transfers[i].Type
+		luckBlock.CreateTime = time.Now()
+		luckBlock.TimeTag = node.TimeTag
+		luckBlock.Category = node.Category
+		luckBlock.PidNode = node.PidNode
+		tmp = append(tmp, luckBlock) //
+	}
+
+	return int(total), tmp
+}
+
+//func removeRepByMap(slc []LuckyBlock) []LuckyBlock { //去除重复的元素
+//	var result []LuckyBlock          //存放返回的不重复切片
+//	tempMap := map[LuckyBlock]byte{} // 存放不重复主键
+//	for _, e := range slc {
+//		l := len(tempMap)
+//		tempMap[e] = 0         //当e存在于tempMap中时，再次添加是添加不进去的，因为key不允许重复
+//		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
+//			result = append(result, e) //当元素不重复时，将元素添加到切片result中
+//		}
+//	}
+//	return result
+//}
+
+// new  old
+/*func saveNewPage(tmp []models.LuckyBlock, totalBlock []models.LuckyBlock) []models.LuckyBlock {
+	var needSave []models.LuckyBlock
+	for i := 0; i < len(tmp); i++ {
+		has := false
+		for j := 0; j < len(totalBlock); j++ {
+			if reflect.DeepEqual(tmp[i], totalBlock[j]) {
+				has = true
+				break
+			}
+		}
+		if !has {
+			needSave = append(needSave, tmp[i])
+		}
+	}
+	return needSave
+}*/
+
+func saveNewPage(tmp []models.LuckyBlock, count int) []models.LuckyBlock {
+	var needSave []models.LuckyBlock
+	for i := len(tmp) - 1; i > len(tmp)-1-count; i-- {
+		needSave = append(needSave, tmp[i])
+	}
+	return needSave
+}
+
+func SlicePage(page, pageSize, nums int) (sliceStart, sliceEnd int) {
+	// 定义page和size的默认值
+	if page <= 0 {
+		page = 1
+	}
+	if pageSize <= 0 {
+		pageSize = 100
+	}
+	// 如果pageSize大于num（切片长度）, 那么sliceEnd直接返回num的值
+	if pageSize > nums {
+		return 0, nums
+	}
+	// 总页数计算，math.Ceil 返回不小于计算值的最小整数（的浮点值）
+	pageCount := int(math.Ceil(float64(nums) / float64(pageSize)))
+	if page > pageCount {
+		return 0, 0
+	}
+	sliceStart = (page - 1) * pageSize
+	sliceEnd = sliceStart + pageSize
+	// 如果页总数比sliceEnd小，那么就把总数赋值给sliceEnd
+	if sliceEnd > nums {
+		sliceEnd = nums
+	}
+	return sliceStart, sliceEnd
+}
+
+func pageCount(total int, pageSize int) int {
+	page := int(math.Ceil(float64(total) / float64(pageSize)))
+	return page
+}
